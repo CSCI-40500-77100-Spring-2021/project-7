51010c5536ffa3dd17dcd0f6ce06fd9efe8fb4bb
lohs <55119191+lohs@users.noreply.github.com>
2021-02-23 23:21:43 -0500

    centered search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = ({ keyword, setKeyword }) => {
+const SearchBar = ({ keyword }) => {
   return (
     <input
       type="text"
       name="SearchBar"
       id="SearchBar"
       placeholder="Enter stock ticker here"
+      value={keyword}
     />
   );
 };
