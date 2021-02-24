511e800bb2d253cc4f26e716af25e9f715c28d1f
lohs <55119191+lohs@users.noreply.github.com>
2021-02-23 22:47:03 -0500

    added free cash flow to display

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,49 +3,44 @@ import "./Display.css";
 
 const Display = ({ keyword, setKeyword }) => {
   return (
-      <div>
-    <table border="1">
-	<tbody>
-		<tr>
-			<td>
-				<h2>
-					Company Name:
-				</h2>
-			</td>
-			<td>
-				<h2>
-					Apple Inc.
-				</h2>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<h2>
-					Ticker Symbol:
-				</h2>
-			</td>
-			<td>
-				<h2>
-					AAPL
-				</h2>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<h2>
-					Current Stock price:
-				</h2>
-			</td>
-			<td>
-				<h2>
-					$123.81
-				</h2>
-			</td>
-		</tr>
-	</tbody>
-</table>
-</div>
-
+    <div>
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>
+              <h2>Company Name:</h2>
+            </td>
+            <td>
+              <h2>Apple Inc.</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>Ticker Symbol:</h2>
+            </td>
+            <td>
+              <h2>AAPL</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>Current Stock price:</h2>
+            </td>
+            <td>
+              <h2>$123.81</h2>
+            </td>
+          </tr>
+		  <tr>
+            <td>
+              <h2>Free Cash Flow per Share:</h2>
+            </td>
+            <td>
+              <h2>$2.06</h2>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
