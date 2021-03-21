2e57a7bbc39ecd10b4fa2fa5e7da2ccd32327b34
Bermet Kydykova <38230942+bermetkydyk@users.noreply.github.com>
2021-03-21 13:44:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/NavBar/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Searchbar from './components/SearchBar/SearchBar';
-import StockDisplay from './components/StockDisplay/StockDisplay'
+import StockDisplay from './components/StockDisplay/StockDisplay';
+import Chart from './Chart';
+// import PolygonApi from './components/PolygonApi';
 
-function App() {
+
+const App =()=> {
+  const [ticker, setTicker]=useState('');
+
   return (
     <React.Fragment>
     <Router>
@@ -15,8 +20,8 @@ function App() {
         <Route path='/' />
       </Switch>
     </Router>
-    <Searchbar />
-    <StockDisplay />
+    <Searchbar setTicker={setTicker}/>
+    <Chart ticker={ticker}/>
     </React.Fragment>
   );
 }
